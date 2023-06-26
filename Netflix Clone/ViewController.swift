51687f4c309d9df006c6499e403b5b6f659a2f62
dev_duynv <duynv.paraline@gmail.com>
2023-06-26 18:17:14 +0700

    [wb #0001 [init project]

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Duy Nguyễn on 26/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
